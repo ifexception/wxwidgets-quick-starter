5f6a88e03b82578fa97b451bddb4ee92ccf2f9b3
Szymon Welgus <szymonwelgus@gmail.com>
2020-09-10 22:30:04 +0200

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,40 @@
+#include <wx/wx.h>
+
+class MainFrame final : public wxFrame
+{
+public:
+    MainFrame()
+        : wxFrame(nullptr, wxID_ANY, wxT("wxWidgets Quick Starter")) {
+        CreateControls();
+    }
+
+    virtual ~MainFrame() = default;
+
+private:
+    void CreateControls() {
+        auto mainPanel = new wxPanel(this, wxID_ANY);
+
+        auto mainSizer = new wxBoxSizer(wxVERTICAL);
+        mainPanel->SetSizer(mainSizer);
+
+        auto starterLabel = new wxStaticText(mainPanel, wxID_ANY, wxT("wxWidgets Quick Starter"));
+        mainSizer->Add(starterLabel, wxSizerFlags().Center());
+    }
+};
+
+class Application : public wxApp
+{
+public:
+    Application() = default;
+    virtual ~Application() = default;
+
+    bool OnInit() override {
+        auto frame = new MainFrame();
+        frame->Show(true);
+        SetTopWindow(frame);
+
+        return true;
+    }
+};
+
+wxIMPLEMENT_APP(Application);
